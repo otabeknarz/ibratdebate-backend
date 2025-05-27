f5d41a12581c685093db407bb48e52020e2756da
Otabek Narz <otabeknarz@gmail.com>
2025-05-27 18:06:44 +0500

    Ticket model added to admin panle

--- debates/admin.py
+++ debates/admin.py
@@ -8,3 +8,10 @@ class DebateAdmin(admin.ModelAdmin):
     list_display = ("region", "district", "date", "time", "is_passed")
     list_filter = ("is_passed",)
     ordering = ("-created_at", "is_passed")
+
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ("id", "debate", "user", "qr_code", "is_checked")
+    list_filter = ("is_checked",)
+    ordering = ("-created_at", "is_checked")
