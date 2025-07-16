559ee06a9a4ded89400e251bac1bc7af5f8b2af6
Otabek Narz <otabeknarz@gmail.com>
2025-07-16 12:14:21 +0500

    imported libs

--- debates/api/views.py
+++ debates/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets, filters
 from rest_framework.decorators import action
+from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 
 from debates.api.serializers import DebateSerializer, TicketSerializer
