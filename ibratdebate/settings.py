e7c4521477e4b222669f49767b7410a3c1f5db83
Otabek Narz <otabeknarz@gmail.com>
2025-08-02 22:30:26 +0500

    made pagination PAGE_SIZE 1000

--- ibratdebate/settings.py
+++ ibratdebate/settings.py
@@ -142,5 +142,5 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.AllowAny",
     ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-    "PAGE_SIZE": 100,
+    "PAGE_SIZE": 1000,
 }
